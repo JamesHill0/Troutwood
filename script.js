1713d75aba74488351fcd4c1aa2bdb45333c7e18
yahmezzz <47001731+yahmezzz@users.noreply.github.com>
2019-12-17 15:04:38 -0800

    All Questions Added
    Questions added to the quiz's question list along with correct answer values stored in dictionary.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var currentQuestion = 0
 var questions = [
-    {prompt:"1) What is the S&P 500?", choices:["500+ of the largest publicly traded companies in the U.S.", "Supersonic stealth drone fighter", "NASCAR race", "A newly discovered comet circling Neptune"]},
-    {prompt:"2) What should be the first line item in your budget?", choices:["1500+ of the largest publicly traded companies in the U.S.", "Supersonic stealth drone fighter", "NASCAR race", "A newly discovered comet circling Neptune"]},
-    {prompt:"3) Which of these companies is in the S&P 500?", choices:["2500+ of the largest publicly traded companies in the U.S.", "Supersonic stealth drone fighter", "NASCAR race", "A newly discovered comet circling Neptune"]}
+    {prompt:"1) What is the S&P 500?", answer:0, choices:["500+ of the largest publicly traded companies in the U.S.", "Supersonic stealth drone fighter", "NASCAR race", "A newly discovered comet circling Neptune"]},
+    {prompt:"2) What should be the first line item in your budget?", answer:0, choices:["Savings", "Phone", "Food", "New shoes"]},
+    {prompt:"3) Which of these companies is in the S&P 500?", answer:0, choices:["Apple", "Stark Industries", "Initech", "Blue Star Airlines", "Monsters Incorporated"]},
+    {prompt:"4) Where is the safest place to keep your money (the least amount of risk)?", answer:1, choices:["Underneath your mattress", "Savings account", "Stock market", "Treasure chest on a remote island... with a map hidden behind a picture on your grandparent's wall."]},
+    {prompt:"5) The most reliable way to earn money is...", answer:3, choices:["To sell fake Louis Vuitton purses", "To scam your friends with emails", "To rob your local bank every other week wearing a different mask", "To get a job"]},
+    {prompt:"6) What is Debt?", answer:2, choices:["a. A 70's rock band", "b. A Clothing line", "c. Borrowed Money", "d. The village elder from the movie Frozen"]},
+    {prompt:"7) What is an interest rate (*challenge question)?", answer:5, choices:["a. The amount of money a bank or entity pays savers or lenders.", "b. The rate a bank or entity charges borrowers.", "c.  A joint investment in agriculture enhancement between Greenland and the United States", "d. Both a and b"]},
+    {prompt:"8) What is a budget?", answer:3, choices:["a) A German automobile", "b) A droid from Star Wars The Rise of Skywalker", "c) A robotic credit card APP with a low APR", "d) A financial plan"]},
+    {prompt:"9) What is a stock?", answer:3, choices:["a) Corn grows on it", "b) Bank Debt", "c) tool used for retrieving golf balls", "d) A security that represents ownership in a company"]},
+    {prompt:"10) Who can invest?", answer:2, choices:["a)  Only companies with greater than one hundred million dollars", "b) Only people with a college degree", "c) Everyone", "d)   Only adults over the age of 21"]}
 ];
+
+ 
 
 
 function addQuestion(qN, animate){
